c93169d4115ed728b5084788b8a4ae507e4080ca
naoki <n.globe.us@gmail.com>
2018-10-09 00:48:52 +0900

    temporay commit

--- htdocs/wordpress/wp-content/themes/code_beauty/index.php
+++ htdocs/wordpress/wp-content/themes/code_beauty/index.php
@@ -2,22 +2,35 @@
 get_header(); ?>
     <main class="col-8">
         <!-- [ 記事のループ ] -->
-	    <?php if ( have_posts() ) { ?>
+	    <?php query_posts('posts_per_page=2'); ?>
+        <?php if ( have_posts() ) : ?>
 		    <?php while ( have_posts() ) : the_post(); ?>
                 <article>
                     <ul class="navbar-nav">
                         <li class="nav-item"><?php  the_time('Y-n-j'); ?></li>
-                        <li class="nav-item"><?php the_category(','); ?></li>
-                        <li class="nav-item"><?php the_tags(); ?></li>
+                        <li class="nav-item"><i class="fas fa-folder"></i><?php the_category( ',' ); ?></li>
+                        <li class="nav-item"><i class="fas fa-tag"></i><?php the_tags( '' ); ?></li>
                     </ul>
                     <a href="<?php the_permalink(); ?>">
                         <h1><?php the_title(); ?></h1>
                     </a>
                 </article>
 		    <?php endwhile; ?>
-	    <?php } ?>
+	    <?php else: ?>
+            <h2>__( 'There is No post.', 'code_beauty' )</h2>
+	    <?php endif; ?>
         <article>
-            <button type="button" class="btn btn-outline-secondary">Older Posts</button>
+	        <?php next_posts_link(); ?>
+	        <?php previous_posts_link(); ?>
+
+            <!--            <button type="button" class="btn btn-outline-secondary">-->
+<!--	        --><?php //the_posts_pagination(
+//		        array(
+//			        'prev_text' => __( 'Older Posts', 'code_beauty' ),
+//			        'next_text' => __( 'Prev Posts', 'code_beauty' ),
+//		        )
+//	        ); ?>
+            <!--            </button>-->
         </article>
     </main>
 <?php
