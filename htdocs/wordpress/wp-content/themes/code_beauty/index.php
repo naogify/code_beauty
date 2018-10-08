e22e121b2c2667650098440c1d8b80b9e3ee40f7
naoki <n.globe.us@gmail.com>
2018-10-08 22:18:09 +0900

    add footer.php

--- htdocs/wordpress/wp-content/themes/code_beauty/index.php
+++ htdocs/wordpress/wp-content/themes/code_beauty/index.php
@@ -101,33 +101,5 @@ get_header(); ?>
             <button type="button" class="btn btn-outline-secondary">Older Posts</button>
         </article>
     </main>
-    <aside class="col-4">
-        widget area
-    </aside>
-</div>
-<footer>
-    <p>
-        <small>&copy;2018 naoki Ohashi <br>code beauty WordPress Theme by naoki</small>
-    </p>
-</footer>
-<script src="js/vendor/modernizr-3.6.0.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"
-        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
-<script src="js/plugins.js"></script>
-<script src="js/main.js"></script>
-
-<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-<script>
-    window.ga = function () {
-        ga.q.push(arguments)
-    };
-    ga.q = [];
-    ga.l = +new Date;
-    ga('create', 'UA-XXXXX-Y', 'auto');
-    ga('send', 'pageview')
-</script>
-<script src="https://www.google-analytics.com/analytics.js" async defer></script>
-</body>
-
-</html>
+<?php
+get_footer();
